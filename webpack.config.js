2556ede3103882cf757ba42e5bd699a923d9f3a7
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-01-07 19:42:14 -0300

    add HtmlWebpackPlugin for HTML file generation in Webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     target: 'web',
@@ -16,5 +17,9 @@ module.exports = {
         },
         port: 3000,
         open: true,
-    }
+    },
+
+    plugins: [new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'index.html')
+    })]
 }
