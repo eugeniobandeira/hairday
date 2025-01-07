eab121a84bd9b0c0569a2cf65d950c52e667137a
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-01-07 20:21:19 -0300

    add form submission handling and set today's date as default in date input

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 "use strict";
 
+// CSS
 import './styles/global.css';
 import './styles/form.css';
 import './styles/schedule.css';
 
+// Config dayjs
 import './libs/dayjs.js';
+
+// JS
+import './modules/form/submit.js';
