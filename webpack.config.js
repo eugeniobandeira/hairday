f46a3ee27c134bf79de8ac6435978137dac809f3
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2025-01-07 19:54:10 -0300

    add SVG assets and configure CopyWebpackPlugin for asset management

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     target: 'web',
@@ -19,10 +20,21 @@ module.exports = {
         open: true,
     },
 
-    plugins: [new HtmlWebpackPlugin({
+    plugins: [
+        new HtmlWebpackPlugin({
         template: path.resolve(__dirname, 'index.html'),
         favicon: path.resolve(__dirname, 'src', 'assets', 'scissors.svg')
-    })],
+        }),
+
+        new CopyWebpackPlugin({
+            patterns: [
+                { 
+                    from: path.resolve(__dirname,'src', 'assets'), 
+                    to: path.resolve(__dirname, 'dist', 'src', 'assets') 
+                }
+            ]
+        })
+    ],
 
     module: {
         rules: [
